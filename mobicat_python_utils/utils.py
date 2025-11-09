0acbd4d99acb9c1b22fe1e844274c39e8bcf80d0
Marc Bosch Manzano <marc.bosch03@estudiant.upf.edu>
2025-11-09 03:34:33 +0100

    Iteration 2: 3-Time-Series, 5-Graphs advanced.
    I have also generated files of mobility municipalities files of number of displacements by day and month.

--- mobicat_python_utils/utils.py
+++ mobicat_python_utils/utils.py
@@ -130,3 +130,104 @@ def group_by_origin_destination_undirected(mobility_df: pd.DataFrame) -> pd.Data
         .sum()
         .reset_index()
     )
+
+
+WEEK_COLOR: list[tuple] = [
+    ('Lunes',     '#08306b'), 
+    ('Martes',    '#2171b5'),  
+    ('Miércoles', '#4292c6'),  
+    ('Jueves',    '#6baed6'), 
+    ('Viernes',   '#9ecae1'),  
+    ('Sábado',    '#006400'), 
+    ('Domingo',   '#32CD32')
+]
+
+MONTH_COLOR: list[tuple] = [
+    ("01", "#182F5D"),
+    ("02", "#29509E"),
+    ("03", "#4784FF"),
+    ("04", "#165623"),
+    ("05", "#27993D"),
+    ("06", "#3FFF65"),
+    ("07", "#514816"),
+    ("08", "#928229"),
+    ("09", "#D9C13D"),
+    ("10", "#792929"),
+    ("11", "#B53C3C"),
+    ("12", "#FF7676"),
+]
+
+YEAR_COLOR: list[tuple] = [
+    ("2023", "#29509E"),
+    ("2024", "#928229"),
+    ("2025", "#B53C8F")
+]
+
+def get_week_color(week_color: tuple[str, str] = WEEK_COLOR) -> pd.DataFrame:
+    return pd.DataFrame(data=week_color, columns=["day_of_week", "day_of_week_color"])
+
+def get_datasets_names(all_datasets_directory: str) -> pd.DataFrame:
+
+    if not os.path.exists(all_datasets_directory):
+        raise Exception("Path %s does not exists" % all_datasets_directory)
+
+    datasets_names = []
+    for dirpath, dirnames, filenames in os.walk(all_datasets_directory):
+        if len(dirnames) > 0:
+            continue
+        year_month = str.split(dirpath, "\\")[-1]
+        year, month = tuple(str.split(year_month, "-"))
+        barrios, mun_barrios, municipios = tuple(sorted(filenames))
+        datasets_names.append({
+            "year": year,
+            "month": month,
+            "barrios": os.path.join(all_datasets_directory, year_month, barrios),
+            "mun_barrios": os.path.join(all_datasets_directory, year_month, mun_barrios),
+            "municipios": os.path.join(all_datasets_directory, year_month, municipios)
+        })
+
+    return (pd.DataFrame(datasets_names)
+        .sort_values(by=["year", "month"])
+        .reset_index()
+        .drop(columns="index")
+    )
+
+
+def group_by_day(df: pd.DataFrame) -> pd.DataFrame:
+    return pd.DataFrame(df
+        .groupby(by=['day', 'day_of_week', "month"])["viajes"]
+        .sum()
+        .reset_index()
+    )
+
+
+def group_by_day_full_datasets(full_datasets_directory: str) -> pd.DataFrame:
+    """
+    Creates a dataframe with all datasets of type #3 of Telefonica 
+    "grouped" by day using the sum of "viajes".
+    """
+    datasets_names_df = get_datasets_names(full_datasets_directory)
+
+    concatenated_df = pd.DataFrame(columns=['day', 'day_of_week', "month", "viajes"])
+    for municipios in datasets_names_df["municipios"]:
+        municipios_df = pd.read_csv(municipios)
+        day_municipios_df = group_by_day(municipios_df)
+        concatenated_df = pd.concat([concatenated_df, day_municipios_df])
+    return concatenated_df.reset_index()
+
+
+def filter_by_day(
+        day_df: pd.DataFrame, 
+        start: str = "2023-01-01", 
+        end: str = "2023-01-31"
+    ) -> pd.DataFrame:
+    return day_df[(day_df["day"] >= pd.Timestamp(start)) 
+                  & (day_df["day"] <= pd.Timestamp(end))]     
+
+def filter_day_by_year_month(
+        day_df: pd.DataFrame, 
+        year: int = 2023, 
+        month: int = 1
+    ) -> pd.DataFrame:
+    return day_df[(day_df["day"].dt.year == year) 
+                  & (day_df["day"].dt.month == month)]
