9c8dcd5264d66c9992f0aa9de540e6a591500e7e
Marc Bosch Manzano <marc.bosch03@estudiant.upf.edu>
2025-11-10 14:27:55 +0100

    Iteration 2: Exploratory Analysis. 2-DataSet-Structuring created.

--- mobicat_python_utils/utils.py
+++ mobicat_python_utils/utils.py
@@ -7,6 +7,38 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from typing import Literal
 
+WEEK_COLOR: list[tuple] = [
+    ('Lunes',     '#08306b'), 
+    ('Martes',    '#2171b5'),  
+    ('Miércoles', '#4292c6'),  
+    ('Jueves',    '#6baed6'), 
+    ('Viernes',   '#9ecae1'),  
+    ('Sábado',    '#006400'), 
+    ('Domingo',   '#22CD32')
+]
+
+MONTH_COLOR: list[tuple] = [
+    ("01", "#182F5D"),
+    ("02", "#29509E"),
+    ("03", "#4784FF"),
+    ("04", "#165623"),
+    ("05", "#27993D"),
+    ("06", "#2FFF65"),
+    ("07", "#514816"),
+    ("08", "#928229"),
+    ("09", "#D9C13D"),
+    ("10", "#792929"),
+    ("11", "#B53C3C"),
+    ("12", "#FF7676"),
+]
+
+YEAR_COLOR: list[tuple] = [
+    ("2023", "#29509E"),
+    ("2024", "#928229"),
+    ("2025", "#B53C8F")
+]
+
+
 def install_if_missing(package):
     try:
         importlib.import_module(package)
@@ -34,7 +66,7 @@ def group_by_municipality_type(df: pd.DataFrame, _type: Literal["origen", "desti
     Reduces the mobility dataframe to origin or destination municipalities.
 
     Parameters:
-        df (DataFrame): A dataframe of type #3 (Movilidad Municipios).
+        df (DataFrame): A dataframe of type #2 (Movilidad Municipios).
         _type (Literal): A string that must be "origen" or "destino".
 
     Returns:
@@ -56,7 +88,7 @@ def group_by_municipality(df: pd.DataFrame) -> pd.DataFrame:
     Reduces the mobility dataframe to municipalities.
 
     Parameters:
-        df (DataFrame): A dataframe of type #3 (Movilidad Municipios).
+        df (DataFrame): A dataframe of type #2 (Movilidad Municipios).
 
     Returns:
         DataFrame: A dataframe grouped by municipalities with columns
@@ -89,7 +121,7 @@ def group_by_origin_destination_directed(mobility_df: pd.DataFrame) -> pd.DataFr
     Reduces the mobility dataframe to ordered pairs of municipalities.
 
     Parameters:
-        df (DataFrame): A dataframe of type #3 (Movilidad Municipios).
+        df (DataFrame): A dataframe of type #2 (Movilidad Municipios).
 
     Returns:
         DataFrame: A dataframe with columns ``"municipio_origen"``, ``"municipio_destino"``,
@@ -106,13 +138,12 @@ def group_by_origin_destination_undirected(mobility_df: pd.DataFrame) -> pd.Data
     Reduces the mobility dataframe to unordered pairs of municipalities.
 
     Parameters:
-        df (DataFrame): A dataframe of type #3 (Movilidad Municipios).
+        df (DataFrame): A dataframe of type #2 (Movilidad Municipios).
 
     Returns:
         DataFrame: A dataframe with columns ``"municipio_origen"``, ``"municipio_destino"``,
         ``"viajes"``, where ``"viajes"`` is the total number of trips for this unordered pair.
     """
-
     directed_edges_df = group_by_origin_destination_directed(mobility_df)
 
     undirected_edges = []
@@ -133,108 +164,82 @@ def group_by_origin_destination_undirected(mobility_df: pd.DataFrame) -> pd.Data
         .reset_index()
     )
 
-
-WEEK_COLOR: list[tuple] = [
-    ('Lunes',     '#08306b'), 
-    ('Martes',    '#2171b5'),  
-    ('Miércoles', '#4292c6'),  
-    ('Jueves',    '#6baed6'), 
-    ('Viernes',   '#9ecae1'),  
-    ('Sábado',    '#006400'), 
-    ('Domingo',   '#32CD32')
-]
-
-MONTH_COLOR: list[tuple] = [
-    ("01", "#182F5D"),
-    ("02", "#29509E"),
-    ("03", "#4784FF"),
-    ("04", "#165623"),
-    ("05", "#27993D"),
-    ("06", "#3FFF65"),
-    ("07", "#514816"),
-    ("08", "#928229"),
-    ("09", "#D9C13D"),
-    ("10", "#792929"),
-    ("11", "#B53C3C"),
-    ("12", "#FF7676"),
-]
-
-YEAR_COLOR: list[tuple] = [
-    ("2023", "#29509E"),
-    ("2024", "#928229"),
-    ("2025", "#B53C8F")
-]
-
 def get_week_color(week_color: tuple[str, str] = WEEK_COLOR) -> pd.DataFrame:
     return pd.DataFrame(data=week_color, columns=["day_of_week", "day_of_week_color"])
 
-def get_datasets_names(all_datasets_directory: str) -> pd.DataFrame:
-
+def get_datasets_names(all_datasets_directory: str) -> list:
+    """
+    """
     if not os.path.exists(all_datasets_directory):
         raise Exception("Path %s does not exists" % all_datasets_directory)
 
+    file_types = ["barrios", "mun_barrios", "municipios"]
     datasets_names = []
     for dirpath, dirnames, filenames in os.walk(all_datasets_directory):
         if len(dirnames) > 0:
             continue
         year_month = str.split(dirpath, "\\")[-1]
         year, month = tuple(str.split(year_month, "-"))
-        barrios, mun_barrios, municipios = tuple(sorted(filenames))
-        datasets_names.append({
-            "year": year,
-            "month": month,
-            "barrios": os.path.join(all_datasets_directory, year_month, barrios),
-            "mun_barrios": os.path.join(all_datasets_directory, year_month, mun_barrios),
-            "municipios": os.path.join(all_datasets_directory, year_month, municipios)
-        })
+        dataset_dir = os.path.join(all_datasets_directory, year_month)
+        file_paths = [os.path.join(dataset_dir, f) for f in sorted(filenames)]
+        file_sizes = [os.path.getsize(f) for f in file_paths]
+        for i in range(len(file_types)):
+            datasets_names.append({
+                "year": year,
+                "month": month,
+                "type": file_types[i],
+                "path": file_paths[i],
+                "size_in_bytes": file_sizes[i]
+            })
+    return datasets_names
 
+def get_datasets_names_df(all_datasets_directory: str) -> pd.DataFrame:
+    """
+    """
+    datasets_names = get_datasets_names(all_datasets_directory)
     return (pd.DataFrame(datasets_names)
         .sort_values(by=["year", "month"])
         .reset_index()
         .drop(columns="index")
+        .astype({"year": "int16", "month": "int8"})
     )
 
-
-def group_by_day(df: pd.DataFrame) -> pd.DataFrame:
-    return pd.DataFrame(df
-        .groupby(by=['day', 'day_of_week', "month"])["viajes"]
-        .sum()
-        .reset_index()
-    )
-
-def group_by_origin_profile(df: pd.DataFrame) -> pd.DataFrame:
-    return pd.DataFrame(df
-        .groupby(by=["origen"])["viajes"]
-        .sum()
-        .reset_index()                 
-    )
-
-def group_by_origin_profile_full_datasets(full_datasets_directory: str) -> pd.DataFrame:
+def full_datasets_groupby(
+        datasets_names_df: pd.DataFrame, 
+        by: list[str], 
+        verbose: bool = False
+    ) -> pd.DataFrame:
     """
-    Creates a dataframe with all datasets of type #3 of Telefonica 
-    "grouped" by "origen" using the sum of "viajes".
     """
-    datasets_names_df = get_datasets_names(full_datasets_directory)
-    concatenated_df = pd.DataFrame(columns=["origen", "viajes"])
-    for municipios in datasets_names_df["municipios"]:
-        municipios_df = pd.read_csv(municipios)
-        origin_profile_df = group_by_origin_profile(municipios_df)
-        concatenated_df = pd.concat([concatenated_df, origin_profile_df])
-    return concatenated_df.reset_index()
+    day_full_df = pd.DataFrame(columns=by + ["viajes"])
+    for dataset_name in datasets_names_df["path"]:
+        print("Processing file %s" % (dataset_name)) if verbose else None
+        df = pd.read_csv(dataset_name, dtype=str)
+        df = df.astype({"viajes": "int"})
+        day_df = df.groupby(by)["viajes"].sum().reset_index()
+        day_full_df = pd.concat([day_full_df, day_df])
+    return day_full_df.reset_index().drop(columns="index")
 
 
-def group_by_day_full_datasets(full_datasets_directory: str) -> pd.DataFrame:
+def full_datasets_filter_zeros(
+        datasets_paths_df: pd.DataFrame, 
+        verbose: bool = False
+    ) -> pd.DataFrame:
     """
-    Creates a dataframe with all datasets of type #3 of Telefonica 
-    "grouped" by "day" using the sum of "viajes".
     """
-    datasets_names_df = get_datasets_names(full_datasets_directory)
-    concatenated_df = pd.DataFrame(columns=['day', 'day_of_week', "month", "viajes"])
-    for municipios in datasets_names_df["municipios"]:
-        municipios_df = pd.read_csv(municipios)
-        day_municipios_df = group_by_day(municipios_df)
-        concatenated_df = pd.concat([concatenated_df, day_municipios_df])
-    return concatenated_df.reset_index()
+    for dataset_path in datasets_paths_df["path"]:
+        print("Processing file %s" % (dataset_path)) if verbose else None
+        df = pd.read_csv(dataset_path, dtype=str)
+        df = df.astype({"viajes": "int"})
+        filtered_df = df[df["viajes"] > 0]
+        filtered_df = filtered_df.reset_index()
+        dirname = os.path.dirname(dataset_path)
+        basename = os.path.basename(dataset_path).split(".")
+        name, extension = "".join(basename[0:-1]), basename[-1]
+        filepath = os.path.join(r".\test-data", name + "_filtered." + extension)
+        filtered_df.drop(columns=["month", "index"]).to_csv(filepath, index=False)
+        print("Original size %d, Filtered size %d, Stored in %s" %
+             (df.shape[0], filtered_df.shape[0], filepath))
 
 
 def filter_by_day(
